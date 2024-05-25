2e222647dd00f45426b26ef739f8570e92796e39
Redouan Cherrat <recherra@c2r5p2.1337.ma>
2024-05-25 14:06:24 +0100

    preprod

---
+++ philo/cmake-build-debug/CMakeFiles/philo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/philo.dir/init.c.o"
+  "philo"
+  "philo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/philo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
