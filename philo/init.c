2e4d0964d1de276a1588fce5f9114b19da6c180f
Redouan Cherrat <recherra@c2r5p2.1337.ma>
2024-07-05 05:38:41 +0100

    preprod

--- philo/init.c
+++ philo/init.c
@@ -12,68 +12,157 @@
 
 #include "init.h"
 
-int ft_atoi(char *str) {
-    int result = 0;
-    int i = 0;
+int	ft_atoi(char *str)
+{
+	int	result;
+	int	i;
 
-    while (str[i] == ' ')
-        i++;
-    while (str[i] == '+' || str[i] == '-')
-    {
-        if (str[i] == '-' || str[i + 1] == '+')
-            return -1;
-        else if (str[i] == '+')
-        {
-            i++;
-            break;
-        }
-        i++;
-    }
-
-    while (str[i] >= '0' && str[i] <= '9')
-    {
-        result *= 10;
-        result += str[i] - '0';
-        i++;
-    }
-    if (str[i])
-        return -1;
-    return result;
+	result = 0;
+	i = 0;
+	while (str[i] == ' ')
+		i++;
+	while (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-' || str[i + 1] == '+')
+			return (-1);
+		else if (str[i] == '+')
+		{
+			i++;
+			break ;
+		}
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result *= 10;
+		result += str[i] - '0';
+		i++;
+	}
+	if (str[i])
+		return (-1);
+	return (result);
 }
 
-int sanitize(int ac, char **av, t_shared *shared)
+int	sanitize(int ac, char **av, t_shared *shared)
 {
-    if ((shared->n_philos = ft_atoi(av[1])) == -1)
-        return 1;
-    else if ((shared->ttd = ft_atoi(av[2])) == -1)
-        return 1;
-    else if ((shared->tte = ft_atoi(av[3])) == -1)
-        return 1;
-    else if ((shared->tts = ft_atoi(av[4])) == -1)
-        return 1;
-    else if (ac == 6)
-    {
+	if ((shared->n_philos = ft_atoi(av[1])) == -1)
+		return (1);
+	else if ((shared->ttd = ft_atoi(av[2])) == -1)
+		return (1);
+	else if ((shared->tte = ft_atoi(av[3])) == -1)
+		return (1);
+	else if ((shared->tts = ft_atoi(av[4])) == -1)
+		return (1);
+	else if (ac == 6)
+	{
         if ((shared->must_eat_times = ft_atoi(av[5])) == -1)
-            return 1;
+        {
+            return (1);
+        }
+
     }
-    return 0;
+	return (0);
 }
 
-int main (int ac, char **av)
+void	*routine(void *arg)
 {
-    t_shared *shared;
+	(void)arg;
+	printf("testing\n");
+	return (NULL);
+}
 
-    if (ac != 5 && ac != 6)
+int	main(int ac, char **av)
+{
+	t_shared *shared;
+
+	// parsing begin
+	if (ac != 5 && ac != 6)
+	{
+		printf("You should provide more/less args!\n");
+		return (1);
+	}
+	shared = malloc(sizeof(t_shared));
+	if (sanitize(ac, av, shared))
+	{
+		printf("Args are not valid!\n");
+		return (1);
+	}
+
+
+	// printf("philos: %d\nttd: %d\ntte: %d\ntts: %d\nmeals: %d\n",
+//			shared->n_philos, shared->ttd, shared->tte, shared->tts,
+//			shared->must_eat_times);
+	// parsing end
+
+	//initialize
+
+	//  allocate philos
+	shared->philos = malloc(shared->n_philos * sizeof(t_ph));
+	if (!shared->philos)
+		return (1);
+
+    //	allocate forks --> mutexs
+	shared->forks = malloc(shared->n_philos * sizeof(t_fork));
+	if (!shared->forks)
+		return (1);
+
+	//initialize forks;
+	int i = 0;
+	 while (i < shared->n_philos)
+	{
+		pthread_mutex_init(&shared->forks[i].fork, NULL);
+		i++;
+	}
+
+
+//	 initialize philosopher
+	i = 0;
+	while (i < shared->n_philos)
+	{
+		shared->philos[i].id = i + 1; //id
+		shared->philos[i].meals = 0; //meals counter
+		shared->philos[i].shared = shared; //shared
+//		assign forks
+
+
+        if ((shared->philos[i].id % 2) == 0)
+        {
+            shared->philos[i].right_fork = &shared->forks[shared->philos[i].id - 1];
+            shared->philos[i].left_fork = &shared->forks[(i + 1) % shared->n_philos];
+        }
+        else
+        {
+            shared->philos[i].left_fork = &shared->forks[shared->philos[i].id - 1];
+            shared->philos[i].right_fork = &shared->forks[(i + 1) % shared->n_philos];
+        }
+		i++;
+	}
+
+	//actual routine
+
+    shared->must_eat_times = -1;
+
+	if (shared->must_eat_times == 0)
+	    return 0;
+    else if (shared->n_philos == 1)
+        return 0;
+    else
     {
-        printf("You should provide more/less args!\n");
-        return 1;
-    }
-    shared = malloc(sizeof(t_shared));
-    if (sanitize(ac, av, shared))
-    {
-        printf("Args are not valid!\n");
-        return 1;
+        int i = 0;
+        while (i < shared->n_philos)
+        {
+            pthread_create(&shared->philos[i].thread, NULL, routine, &shared->philos[i]);
+            i++;
+        }
     }
 
-    return 0;
+
+	//join
+	i = 0;
+	while (i < shared->n_philos)
+	{
+		pthread_join(shared->philos[i].thread, NULL);
+		i++;
+	}
+	return (0);
 }
--- philo/init.h
+++ philo/init.h
@@ -11,45 +11,43 @@
 /* ************************************************************************** */
 
 #ifndef INIT_H
-#define INIT_H
+# define INIT_H
 
-#include <stdio.h> //printf
-#include <string.h> //memset
-#include <stdlib.h> // malloc, free
-#include <unistd.h> //usleep
-#include <sys/time.h> //gettimeofday
-#include <pthread.h> //pthread: create, detach, join | mutex: init, lock, unlock, destroy
+# include <pthread.h>  //pthread: create, detach, join | mutex: init, lock,	unlock, destroy
+# include <stdio.h>    //printf
+# include <stdlib.h>   // malloc, free
+# include <string.h>   //memset
+# include <sys/time.h> //gettimeofday
+# include <unistd.h>   //usleep
 
-typedef struct s_shared t_shared;
+typedef struct s_shared	t_shared;
 
 typedef struct s_fork
 {
-    pthread_mutex_t fork;
-    int id;
-} t_fork;
+	pthread_mutex_t		fork;
+	int					id;
+}						t_fork;
 
 typedef struct s_ph
 {
-    int id;
-    int meals;
-    pthread_t thread;
-    t_fork *right_fork;
-    t_fork *left_fork;
-    t_shared *shared;
-} t_ph;
-
+	int					id;
+	int					meals;
+	pthread_t			thread;
+	t_fork				*right_fork;
+	t_fork				*left_fork;
+	t_shared			*shared;
+}						t_ph;
 
 typedef struct s_shared
 {
-    int n_philos;
-    int ttd;
-    int tte;
-    int tts;
-    int must_eat_times;
-    int start_time;
-    t_ph *philos;
-    t_fork *forks;
-} t_shared;
-
+	int					n_philos;
+	int					ttd;
+	int					tte;
+	int					tts;
+	int					must_eat_times;
+	int					start_time;
+	t_ph				*philos;
+	t_fork				*forks;
+}						t_shared;
 
 #endif
